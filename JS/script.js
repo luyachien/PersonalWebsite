246f1c6e615c29a4e7189144d7aa8f7afb3c03b1
luyachien <luya.chien@gmail.com>
2024-07-18 21:36:01 +0800

    設定回到頂部的按鈕的JS：讓回到頂部的按鈕在頁面頂部時不會出現，滑鼠滑超過20px時才會顯示按鈕

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,4 @@
+/*HamburgerToggle*/
 document.getElementById('hamburgerToggle').addEventListener('click', function() {
     this.classList.toggle('open');
     var navbarToggler = document.getElementById('navbarTogglerDemo02');
@@ -9,3 +10,20 @@ document.getElementById('hamburgerToggle').addEventListener('click', function() 
         navbarToggler.classList.add('collapse');
     }
 });
+
+
+/*GoBackToTop*/
+// 取得回到頂部按鈕元素
+var backToTopButton = document.getElementById('page-top');
+
+// 監聽頁面滾動事件
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    // 如果頁面滾動超過 20px，則顯示回到頂部按鈕
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+}
